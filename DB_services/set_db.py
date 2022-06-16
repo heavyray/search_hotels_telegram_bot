d2e5fa0e885e62b43415eeb688f38927d964478e
heavyray <98265533+heavyray@users.noreply.github.com>
2022-06-16 12:44:40 +0300

    Update set_db.py

--- DB_services/set_db.py
+++ DB_services/set_db.py
@@ -2,7 +2,7 @@ import sqlite3
 
 
 def create_db():
-    conn = sqlite3.connect('../db.db', check_same_thread=False)
+    conn = sqlite3.connect('../db.sqlite3', check_same_thread=False)
     cursor = conn.cursor()
 
     cursor.execute("""CREATE TABLE IF NOT EXISTS history (
